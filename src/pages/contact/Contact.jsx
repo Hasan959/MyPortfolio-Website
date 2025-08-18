b56f63038895492080e6dc9392e9e56f16cce9f9
Hasan Mahmud <hasanmahmud.biddut@gmail.com>
2025-08-18 23:01:31 +0600

    thirteen

--- src/pages/contact/Contact.jsx
+++ src/pages/contact/Contact.jsx
@@ -1,8 +1,60 @@
-import { RiMap2Line ,RiUser3Line ,RiMailLine ,RiBook2Line ,RiSendPlaneLine } from "react-icons/ri";
+import { RiMap2Line, RiUser3Line, RiMailLine, RiBook2Line, RiSendPlaneLine } from "react-icons/ri";
 import './contact.css'
+import { useState } from "react";
+import  emailjs from "@emailjs/browser";
+
 
 
 const Contact = () => {
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    subject: '',
+    message: '',
+  });
+
+  const [feedbackMessage, setFeedbackMessage] = useState('');
+  const [messageColor, setMessageColor] = useState('');
+
+  const handleChange = (e) => {
+    setFormData({...formData, [e.target.name]: e.target.value});
+
+  };
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    if(
+      !formData.name || 
+      !formData.email ||
+      !formData.subject ||
+      !formData.message
+    ) {
+      setMessageColor('color-red');
+      setFeedbackMessage('write all the input fields');
+      
+      
+      setTimeout(()=>setFeedbackMessage(''),3000)
+
+     return;
+    }
+     emailjs
+      .send('service_ampbtj8','template_vh2bb1m', formData,'y7GAeVyjDvNEgCT7J' 
+        
+      )
+      .then(
+        () => {
+          setMessageColor('color-first');
+          setFeedbackMessage('Message sent ✔');
+
+          setTimeout(()=>setFeedbackMessage(''),5000);
+        },
+        (error) => {
+         alert('OOPs! SOMETHING WENT WRONG...',error);
+        },
+      );
+  };
+
   return (
     <section className='contact section'>
       <h2 className='section-title'>
@@ -64,50 +116,78 @@ const Contact = () => {
 
         </div>
 
-        <form action="" className="contact-form grid">
+        <form action="" className="contact-form grid " onSubmit={sendEmail}>
 
           <div className="contact-form-group grid">
 
-             <div className="contact-form-div">
-                <label htmlFor="" className="contact-form-label">
-                  Your full Name
-                </label>
-                <input type="text" name="name" className="contact-form-input" />
-             </div>
+            <div className="contact-form-div">
+              <label htmlFor="" className="contact-form-label">
+                Your full Name
+              </label>
+              <input 
+               type="text"
+               name="name" 
+               onChange={handleChange} 
+               value={formData.name}
+               className="contact-form-input" />
+            </div>
 
-             <div className="contact-form-div">
-                <label htmlFor="" className="contact-form-label">
-                  Your Email Address 
-                </label>
-                <input type="email" name="email" className="contact-form-input" />
+            <div className="contact-form-div">
+              <label htmlFor="" className="contact-form-label">
+                Your Email Address
+              </label>
+              <input 
+              type="email" 
+              name="email" 
+              onChange={handleChange}
+              value={formData.email}
+              className="contact-form-input"
+              />
             </div>
 
           </div>
 
           <div className="contact-form-div">
 
-                <label htmlFor="" className="contact-form-label">
-                  Your Subject <b>*</b>
-                  </label>
-                <input type="email" name="email" className="contact-form-input" />
+            <label htmlFor="" className="contact-form-label">
+              Your Subject <b>*</b>
+            </label>
+            <input 
+            type="text" 
+            name="subject" 
+            onChange={handleChange}
+            value={formData.subject}
+            className="contact-form-input" />
 
           </div>
+
           <div className="contact-form-div">
 
-                <label htmlFor="" className="contact-form-label">
-                  Your Message <b>*</b>
-                </label>
-                <textarea name="message" className="contact-form-input contact-form-area" id=""></textarea>
-                <input type="email" name="email" className="contact-form-input" />
+            <label htmlFor="" className="contact-form-label">
+              Your Message <b>*</b>
+            </label>
+            <textarea 
+            name="message" 
+            onChange={handleChange}
+            value={formData.message}
+            className="contact-form-input contact-form-area" 
+            id="">
+            </textarea>
 
           </div>
+
           <div className="contact-button">
             <button className="button">
               Send Message <span className="button-icon"><RiSendPlaneLine /></span>
             </button>
           </div>
-        </form>
 
+          {feedbackMessage && (
+            <p className={`contact-message ${messageColor}`}>
+              {feedbackMessage}
+            </p>
+          ) }
+        </form>
       </div>
     </section>
   )
